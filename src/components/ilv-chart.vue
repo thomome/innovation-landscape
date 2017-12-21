a8bded248e862c39a78768b3590a9fcdf014c2c1
Thomas Guthruf <thomasguthruf@gmail.com>
2017-12-21 18:04:40 +0100

    added axis and bars

--- src/components/ilv-chart.vue
+++ src/components/ilv-chart.vue
@@ -1,20 +1,98 @@
 <template>
-  <svg width="100%" height="500px" ref="chart">
-    <g class="bars">
-      <ilv-chart-bar from="0" to="250" amount="250"></ilv-chart-bar>
-    </g>
-  </svg>
+  <div class="chart-container">
+    <svg width="100%" height="500px" ref="chart">
+      <ilv-chart-axis direction="y"></ilv-chart-axis>
+      <g class="bars">
+        <ilv-chart-bar
+          v-for="inst in instruments"
+          :key="'instrument-' + inst.id"
+          :from="inst.from"
+          :to="inst.to"
+          :amount="inst.amount"
+        ></ilv-chart-bar>
+      </g>
+    </svg>
+  </div>
 </template>
 
 <script>
   import ilvChartBar from './ilv-chart-bar.vue'
+  import ilvChartAxis from './ilv-chart-axis.vue'
 
   export default {
-    components: {
-      ilvChartBar
-    },
-    data () {
+    data() {
       return {}
+    },
+    components: {
+      ilvChartBar, ilvChartAxis
+    },
+    computed: {
+      instruments() {
+        const amounts = {}
+        const totals = {}
+        const years = this.$store.state.year.selected
+
+        this.$store.getters.budgets.forEach((o, i) => {
+          if(!amounts[o.instrumentId]) {
+            amounts[o.instrumentId] = []
+            totals[o.instrumentId] = 0
+          }
+          if(years.indexOf(o.year) !== -1 || years.length === 0){
+            amounts[o.instrumentId].push(o)
+            totals[o.instrumentId] += o.amount
+          }
+        })
+        let maxAmount = 0
+        const instruments = this.$store.getters.instrumentsSelected.map(o => {
+          if(totals[o.id] > maxAmount) maxAmount = totals[o.id]
+          o.amounts = amounts[o.id]
+          o.amount = totals[o.id]
+          return o
+        })
+
+        const maxLength = maxAmount.toString().length
+        let maxCeil = Math.floor(maxAmount/Math.pow(10, maxLength-2))
+        maxCeil = Math.ceil(maxCeil/5)*5*(Math.pow(10, maxLength-2))
+        
+        this.$store.commit('setChartMaxAmount', {
+          maxAmount: maxCeil
+        })
+
+        return instruments
+      }
+    },
+    methods: {
+      updateSize() {
+        this.$store.commit('setChartDimension', {
+          width: this.$refs.chart.clientWidth,
+          height: this.$refs.chart.clientHeight
+        })
+
+      }
+    },
+    mounted: function () {
+      this.updateSize()
+      window.addEventListener('resize', this.updateSize)
+    },
+    beforeDestroy: function () {
+      window.removeEventListener('resize', this.updateSize)
     }
   }
 </script>
+
+<style lang="scss" scoped>
+  .chart-container {
+    position: relative;
+    width: 100%;
+    height: 0;
+    padding-bottom: 66.66%;
+
+    svg {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+    }
+  }
+</style>
