38175534449e3830e37fadeac8ca0bdc7967ded8
Morme <thomasguthruf@gmail.com>
2017-12-23 14:28:41 +0100

    instruments moved to store

--- src/components/ilv-chart.vue
+++ src/components/ilv-chart.vue
@@ -29,65 +29,7 @@
     },
     computed: {
       instruments() {
-        const amounts = {}
-        const totals = {}
-        const years = this.$store.state.year.selected
-
-        this.$store.getters.budgets.forEach((o, i) => {
-          if(!amounts[o.instrumentId]) {
-            amounts[o.instrumentId] = []
-            totals[o.instrumentId] = 0
-          }
-          if(years.indexOf(o.year) !== -1 || years.length === 0){
-            amounts[o.instrumentId].push(o)
-            totals[o.instrumentId] += o.amount
-          }
-        })
-        let maxAmount = 0
-        const instruments = this.$store.getters.instrumentsSelected.map(o => {
-          if(totals[o.id] > maxAmount) maxAmount = totals[o.id]
-          o.amounts = amounts[o.id]
-          o.amount = totals[o.id]
-          return o
-        })
-
-        const maxLength = maxAmount.toString().length
-
-        let simpleMax = maxAmount/Math.pow(10, maxLength-2)*0.1
-
-        const dividers = [0.2, 0.25, 0.5, 1, 2, 2.5];
-        let divider = 1
-        for(let i = 0; i < dividers.length; i++) {
-          const v = dividers[i]
-          const tMax = Math.ceil(simpleMax/v)*v
-          const rest = tMax%v
-          const mult = tMax/v
-          if(rest === 0 && mult < 8 && mult > 3) {
-            divider = v
-            simpleMax = tMax
-            break;
-          }
-        }
-
-        const maxCeil = simpleMax*Math.pow(10, maxLength-2)*10
-
-        const units = this.$store.state.chart.units
-        let unit = 0
-        units.forEach((v, i) => {
-          const place =  Math.floor(maxAmount/Math.pow(10, v.start))
-          if(place >= 1){
-            unit = v.start
-          }
-        })
-
-        this.$store.commit('setChart', {
-          axisMax: maxCeil,
-          axisTics: simpleMax/divider,
-          axisDivider: divider,
-          axisUnit: unit
-        })
-
-        return instruments
+        return this.$store.getters.instruments;
       }
     },
     methods: {
--- src/store.js
+++ src/store.js
@@ -64,18 +64,81 @@ export const store = new Vuex.Store({
     }
   },
   getters: {
-    instruments({ instrument }) {
+    instrumentsAll({ instrument }) {
       return instrument.list.map(id => instrument.data[id])
     },
     instrumentsSelected({ instrument }) {
       if(instrument.selected.length === 0) return instrument.list.map(id => instrument.data[id])
       return instrument.selected.map(id => instrument.data[id])
     },
-    budgets({ budget }) {
+    budgetsAll({ budget }) {
       return budget.list.map(id => budget.data[id])
     },
     years({ year }) {
       return year.list.map(id => year.data[id])
+    },
+
+    instruments(state, getters) {
+      const amounts = {}
+      const totals = {}
+      const years = state.year.selected
+
+      getters.budgetsAll.forEach((o, i) => {
+        if(!amounts[o.instrumentId]) {
+          amounts[o.instrumentId] = []
+          totals[o.instrumentId] = 0
+        }
+        if(years.indexOf(o.year) !== -1 || years.length === 0){
+          amounts[o.instrumentId].push(o)
+          totals[o.instrumentId] += o.amount
+        }
+      })
+
+      let maxAmount = 0
+      const instruments = getters.instrumentsSelected.map(o => {
+        if(totals[o.id] > maxAmount) maxAmount = totals[o.id]
+        o.amounts = amounts[o.id]
+        o.amount = totals[o.id]
+        return o
+      })
+
+      const maxLength = maxAmount.toString().length
+
+      let simpleMax = maxAmount/Math.pow(10, maxLength-2)*0.1
+
+      const dividers = [0.2, 0.25, 0.5, 1, 2, 2.5];
+      let divider = 1
+      for(let i = 0; i < dividers.length; i++) {
+        const v = dividers[i]
+        const tMax = Math.ceil(simpleMax/v)*v
+        const rest = tMax%v
+        const mult = tMax/v
+        if(rest === 0 && mult < 8 && mult > 3) {
+          divider = v
+          simpleMax = tMax
+          break;
+        }
+      }
+
+      const maxCeil = simpleMax*Math.pow(10, maxLength-2)*10
+
+      const units = state.chart.units
+      let unit = 0
+      units.forEach((v, i) => {
+        const place =  Math.floor(maxAmount/Math.pow(10, v.start))
+        if(place >= 1){
+          unit = v.start
+        }
+      })
+
+      store.commit('setChart', {
+        axisMax: maxCeil,
+        axisTics: simpleMax/divider,
+        axisDivider: divider,
+        axisUnit: unit
+      })
+
+      return instruments
     }
   },
   actions: {}
