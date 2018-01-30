0fcbd8e470e8c9a1c4dbf5df7ee95639b4f09d3b
Thomas Guthruf <thomasguthruf@gmail.com>
2018-01-30 18:47:58 +0100

    added eventhub to vue root object

--- src/main.js
+++ src/main.js
@@ -2,12 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import Vuetify from 'vuetify'
 import { store } from './store.js'
+import { eventHub } from './event-hub.js'
 
 import 'vuetify/dist/vuetify.css'
 
 Vue.use(Vuetify, {
   theme: {
     primary: '#069'
+  }
+})
+
+Vue.mixin({
+  data: function () {
+    return {
+      eventHub: eventHub
+    }
   }
 })
 
