9f88f42640651f17adf277b712ab39d181a597cc
Thomas Guthruf <thomasguthruf@gmail.com>
2018-01-30 18:51:49 +0100

    added new filter option for budget type

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,11 @@
     <v-content>
       <v-card flat tile>
         <v-container fluid pa-0>
+          <v-layout row wrap>
+            <v-flex xs12 pa-0>
+              <ilv-chart></ilv-chart>
+            </v-flex>
+          </v-layout>
           <v-layout row wrap justify-end>
             <v-flex xs12 sm6 pa-1>
               <v-select
@@ -12,6 +17,7 @@
                 multiple
                 autocomplete
                 clearable
+                dense
               ></v-select>
             </v-flex>
             <v-flex xs12 sm6 pa-1>
@@ -22,9 +28,10 @@
                 multiple
                 autocomplete
                 clearable
+                dense
               ></v-select>
             </v-flex>
-            <v-flex xs12 sm12 pa-1>
+            <v-flex xs12 sm6 pa-1>
               <v-select
                 label="Instrumente"
                 v-bind:items="instrumentOption"
@@ -32,27 +39,37 @@
                 multiple
                 autocomplete
                 clearable
+                dense
+              ></v-select>
+            </v-flex>
+            <v-flex xs12 sm6 pa-1>
+              <v-select
+                label="Budget"
+                v-bind:items="typeOption"
+                v-model="$store.state.type.selected"
+                multiple
+                autocomplete
+                clearable
+                dense
               ></v-select>
             </v-flex>
 
           </v-layout>
-          <v-layout row wrap>
-            <v-flex xs12 pa-0>
-              <ilv-chart></ilv-chart>
-            </v-flex>
-          </v-layout>
+
         </v-container>
       </v-card>
     </v-content>
+    <ilv-tooltip></ilv-tooltip>
   </v-app>
 </template>
 
 <script>
   import ilvChart from './components/ilv-chart.vue'
+  import ilvTooltip from './components/ilv-tooltip.vue'
 
   export default {
     components: {
-      ilvChart
+      ilvChart, ilvTooltip
     },
     data () {
       return {}
@@ -66,14 +83,18 @@
       },
       regionOption() {
         return this.$store.getters.regionAll.map(o => { return { text: o.en, value: o.id } })
+      },
+      typeOption() {
+        return this.$store.getters.typeAll.map(o => { return { text: o.en, value: o.id } })
       }
     },
     mounted() {
       const loadPhaseTable = this.$store.dispatch('loadPhaseTable')
       const loadCategoryTable = this.$store.dispatch('loadCategoryTable')
       const loadRegionTable = this.$store.dispatch('loadRegionTable')
+      const loadTypeTable = this.$store.dispatch('loadTypeTable')
 
-      Promise.all([ loadPhaseTable, loadCategoryTable, loadRegionTable ]).then(() => {
+      Promise.all([ loadPhaseTable, loadCategoryTable, loadRegionTable, loadTypeTable ]).then(() => {
         this.$store.dispatch('loadInstrumentTable').catch((err) => {
           console.error(err)
         })
