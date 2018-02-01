02df046f709a91afe2e1b8fc6fcc1b5c29682eaf
Thomas Guthruf <thomasguthruf@gmail.com>
2018-02-01 19:08:39 +0100

    changed appearance of tooltip

--- src/components/ilv-tooltip.vue
+++ src/components/ilv-tooltip.vue
@@ -1,12 +1,33 @@
 <template>
   <transition name="fade">
-    <div v-if="active" ref="tooltip" class="tooltip-container" :style="{ transform: 'translate('+(pos.x+offset.x)+'px,'+pos.y+'px)' }">
+    <div
+      v-if="active"
+      ref="tooltip"
+      class="tooltip-container"
+      :style="{
+        transform: 'translate('+(pos.x)+'px,'+pos.y+'px)',
+        marginLeft: offset.x + 'px'
+      }"
+    >
       <div class="tooltip">
-        <h6 class="title">{{ instrument.institution }}<span v-if="instrument.institution">:</span> {{ instrument.instrument }}</h6>
-        <span class="subheadingfont">{{ instrument.categoryIds.map(id => $store.state.category.data[id].en).join(', ')  }}</span>
-        <h4>Budget</h4>
-        <div v-for="budget in instrument.budget" :key="`${instrument.id}-${budget.typeIds.join(',')}`">
-          {{ budget.typeIds.map(id => $store.state.type.data[id].en).join('/') }}: {{ budget.amount }} Fr.
+        <div class="tooltip-title">{{ instrument.institution }} {{ instrument.instrument }}</div>
+        <div class="tooltip-lead">{{ lead }}</div>
+        <div v-for="budget in budgetItems" :key="`${instrument.id}-${budget.typeIds.join(',')}`">
+          <div class="tooltip-budget-bar">
+            <div class="tooltip-budget-bar-fill" :style="{
+              transform: `scaleX(${1/budgetItems[0].amount*budget.amount})`,
+              background: budget.background
+            }"></div>
+          </div>
+          <div class="tooltip-budget-description">
+            <span class="tooltip-budget-name">
+              Budget | <strong>{{ budget.typeString }}</strong>
+            </span>
+            <span class="tooltip-budget-amount">{{ formatAmount(budget.amount) }} Fr.</span>
+          </div>
+        </div>
+        <div class="tooltip-data-source">
+          {{ instrument.source }}
         </div>
       </div>
     </div>
@@ -14,12 +35,15 @@
 </template>
 
 <script>
+import { formatAmount, intersect } from './../util.js'
+
 export default {
   data() {
     return {
       instrumentId: null,
       pos: { x: 10, y: 10 },
-      offset: { x: 0, y: 0 }
+      offset: { x: 0, y: 0 },
+      patternSize: 5
     }
   },
   computed: {
@@ -28,7 +52,39 @@ export default {
     },
     instrument() {
       return this.$store.state.instrument.data[this.instrumentId]
+    },
+    budgetItems() {
+      let budgetItems = this.instrument.budget.filter((v) => {
+        return intersect(v.typeIds, this.$store.getters.typeSelected).length !== 0
+      })
+      budgetItems = budgetItems.map((v) => {
+        const item = v
+        item.typeString = v.typeIds.map(id => this.$store.state.type.data[id].en).join('/')
+        if(v.typeIds.length === 1) {
+          item.background = this.$store.state.type.data[v.typeIds[0]].color
+        } else {
+          const gradientArray = []
+          v.typeIds.forEach((id, index) => {
+            const color = this.$store.state.type.data[id].color
+            gradientArray.push(`${color} ${index*this.patternSize}px`)
+            gradientArray.push(`${color} ${(index+1)*this.patternSize}px`)
+          })
+          const gradient = `repeating-linear-gradient(30deg, ${gradientArray.join(', ')})`
+          console.log(gradient)
+          item.background = gradient
+        }
+        return item
+      })
+      return budgetItems.sort((a, b) => {
+        return a.amount == b.amount ? a.typeIds[0] - b.typeIds[0] : b.amount - a.amount
+      })
+    },
+    lead() {
+      return this.$store.state.region.data[this.instrument.regionId].en + ' » ' + this.instrument.categoryIds.map(id => this.$store.state.category.data[id].en).join(', ')
     }
+  },
+  methods: {
+    formatAmount: formatAmount
   },
   mounted() {
     window.addEventListener('mousemove', (e) => {
@@ -36,7 +92,7 @@ export default {
         if(this.$refs.tooltip) {
           const width = this.$refs.tooltip.clientWidth
           if(e.pageX + width > document.body.clientWidth) {
-            this.offset.x = width * -1
+            this.offset.x = (e.pageX + width - document.body.clientWidth) * -1
           } else {
             this.offset.x = 0
           }
@@ -75,12 +131,66 @@ export default {
     position: absolute;
     margin-left: 15px;
     margin-top: 30px;
+    min-width: 300px;
+    max-width: 350px;
 
     .tooltip {
+      position: relative;
       padding: 20px;
       background: #fff;
       border: 2px solid #efefef;
-      box-shadow: 2px 2px 10px rgba(0,0,0,0.1);
+      box-shadow: 2px 5px 10px rgba(0,0,0,0.2);
+
+      .tooltip-title {
+        font-size: 1.28rem;
+        font-weight: bold;
+        line-height: 1;
+      }
+      .tooltip-lead {
+        font-size: 1rem;
+        color: #666;
+        margin-bottom: 1rem;
+      }
+      .tooltip-budget-description {
+        display: flex;
+        width: 100%;
+        margin-bottom: 0.7rem;
+
+        .tooltip-budget-name {
+          margin-right: 1rem;
+        }
+        .tooltip-budget-amount {
+          font-weight: bold;
+          margin-left: auto;
+        }
+      }
+      .tooltip-budget-bar {
+        position: relative;
+        width: 100%;
+        height: 3px;
+        background: #efefef;
+        margin-bottom: 1px;
+
+        .tooltip-budget-bar-fill {
+          width: 100%;
+          background: #000;
+          height: 100%;
+          transform-origin: left top;
+        }
+      }
+      .tooltip-data-source {
+        margin-top: 1.5rem;
+        &::before {
+          content: '';
+          display: block;
+          width: 1.5rem;
+          height: 1px;
+          background: #000;
+          margin-bottom: 3px;
+        }
+        font-size: 0.85rem;
+        font-style: italic;
+      }
     }
   }
 
